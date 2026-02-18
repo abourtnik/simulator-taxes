495c5766498015679f84ba9facf45bbdfd4aa4af
abourtnik <contact@antonbourtnik.fr>
2026-02-18 21:20:46 +0100

    Fix Actions

--- tests/tax.test.js
+++ tests/tax.test.js
@@ -5,6 +5,6 @@ test('calculate family quotient for single person without children', () => {
     expect(calculateFamilyQuotient(false, 0, 0)).toBe(1);
 });
 
-test('calculate family quotient for married person without children', () => {
+test('calculate family quotient for married person without children 2', () => {
     expect(calculateFamilyQuotient(true, 0, 0)).toBe(2);
 });
