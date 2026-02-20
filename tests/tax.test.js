6c6483733144e1e0257ca505f6554fb39e9e9a95
abourtnik <contact@antonbourtnik.fr>
2026-02-20 21:04:48 +0100

    Fix tests

--- tests/tax.test.js
+++ tests/tax.test.js
@@ -1,10 +1,11 @@
 import { expect, test } from 'vitest'
 import { calculateFamilyQuotient } from '@/functions/tax.ts'
+import {Situation} from "@/types/index.ts";
 
 test('calculate family quotient for single person without children', () => {
-    expect(calculateFamilyQuotient(false, 0, 0)).toBe(1);
+    expect(calculateFamilyQuotient(Situation.SINGLE, 0, 0)).toBe(1);
 });
 
 test('calculate family quotient for married person without children', () => {
-    expect(calculateFamilyQuotient(true, 0, 0)).toBe(2);
+    expect(calculateFamilyQuotient(Situation.MARRIED, 0, 0)).toBe(2);
 });
